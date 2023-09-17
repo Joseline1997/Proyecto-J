b0503a8b7e14d2fd81fe556cf72937ff6e65dd6b
Joseline Malena Cedeño Rivera <131705847+Joseline1997@users.noreply.github.com>
2023-09-17 14:54:44 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --username test1 --password 123321 --noinput --email 'blank@email.com'
